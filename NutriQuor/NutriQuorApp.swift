8c0953b98a123852ae7236c8a483ca4999544a68
LtThanh <lamtanthanh2005@gmail.com>
2026-01-12 21:54:22 +0700

    Initial Commit

---
+++ NutriQuor/NutriQuorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriQuorApp.swift
+//  NutriQuor
+//
+//  Created by Lâm Tấn Thành on 12/1/26.
+//
+
+import SwiftUI
+
+@main
+struct NutriQuorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
